b5c734b870e96a42d828827b9eeefd7c9882a550
Michael Todd <eltodd@gmail.com>
2022-02-08 03:05:22 +0000

    Test Updates

--- src/turret_node.cpp
+++ src/turret_node.cpp
@@ -16,47 +16,53 @@ uint32_t config_i = 0;
 
 double turret_target = 0;
 
-Motor Turret_Shooter_Master(13, Motor::Motor_Type::TALON_FX);
-Motor Turret_Shooter_Slave_Motor(14, Motor::Motor_Type::TALON_FX);
-Motor Turret_Yaw_Motor(8, Motor::Motor_Type::TALON_FX);
-Motor Turret_Hood_Motor(12, Motor::Motor_Type::TALON_FX);
+Motor * Turret_Shooter_Master;
+Motor * Turret_Shooter_Slave_Motor;
+Motor * Turret_Yaw_Motor;
+Motor * Turret_Hood_Motor;
 
 void hmi_signal_callback(const hmi_agent_node::HMI_Signals& msg)
 {
     (void) msg;
+    ROS_INFO("GOT INTO HERE EVEN!");
 
-    Turret_Yaw_Motor.set(Motor::Control_Mode::MOTION_MAGIC, 0, 0);
-    Turret_Hood_Motor.set(Motor::Control_Mode::MOTION_MAGIC, 0, 0);
-    Turret_Shooter_Master.set(Motor::Control_Mode::VELOCITY, 0, 0);
+    Turret_Yaw_Motor->set(Motor::Control_Mode::MOTION_MAGIC, 0, 0);
+    Turret_Hood_Motor->set(Motor::Control_Mode::MOTION_MAGIC, 0, 0);
+    Turret_Shooter_Master->set(Motor::Control_Mode::VELOCITY, 0, 0);
 }
 
-void publish_config()
+void config_motors()
 {
-    Turret_Yaw_Motor.config().set_kP(0.67);
-    Turret_Yaw_Motor.config().set_kI(0.0);
-    Turret_Yaw_Motor.config().set_kD(0.92);
-    Turret_Yaw_Motor.config().set_kF(0.047651);
-    Turret_Yaw_Motor.config().set_motion_cruise_velocity(16000);
-    Turret_Yaw_Motor.config().set_motion_acceleration(36000);
-    Turret_Yaw_Motor.config().set_motion_s_curve_strength(5);
+    Turret_Shooter_Master = new Motor(13, Motor::Motor_Type::TALON_FX);
+    Turret_Shooter_Slave_Motor = new Motor(14, Motor::Motor_Type::TALON_FX);
+    Turret_Yaw_Motor = new Motor(8, Motor::Motor_Type::TALON_FX);
+    Turret_Hood_Motor = new Motor(12, Motor::Motor_Type::TALON_FX);
 
-    Turret_Hood_Motor.config().set_kP(0.67);
-    Turret_Hood_Motor.config().set_kI(0.0);
-    Turret_Hood_Motor.config().set_kD(0.92);
-    Turret_Hood_Motor.config().set_kF(0.047651);
-    Turret_Hood_Motor.config().set_motion_cruise_velocity(16000);
-    Turret_Hood_Motor.config().set_motion_acceleration(32000);
-    Turret_Hood_Motor.config().set_motion_s_curve_strength(5);
+    Turret_Yaw_Motor->config().set_kP(0.67);
+    Turret_Yaw_Motor->config().set_kI(0.0);
+    Turret_Yaw_Motor->config().set_kD(0.92);
+    Turret_Yaw_Motor->config().set_kF(0.047651);
+    Turret_Yaw_Motor->config().set_motion_cruise_velocity(16000);
+    Turret_Yaw_Motor->config().set_motion_acceleration(36000);
+    Turret_Yaw_Motor->config().set_motion_s_curve_strength(5);
+
+    Turret_Hood_Motor->config().set_kP(0.67);
+    Turret_Hood_Motor->config().set_kI(0.0);
+    Turret_Hood_Motor->config().set_kD(0.92);
+    Turret_Hood_Motor->config().set_kF(0.047651);
+    Turret_Hood_Motor->config().set_motion_cruise_velocity(16000);
+    Turret_Hood_Motor->config().set_motion_acceleration(32000);
+    Turret_Hood_Motor->config().set_motion_s_curve_strength(5);
     
-    Turret_Shooter_Slave_Motor.config().set_follower(true, 13);
-    Turret_Shooter_Slave_Motor.config().set_inverted(true);
+    Turret_Shooter_Slave_Motor->config().set_follower(true, 13);
+    Turret_Shooter_Slave_Motor->config().set_inverted(true);
 
-    Turret_Shooter_Master.config().set_kP(0.03);
-    Turret_Shooter_Master.config().set_kI(0.0);
-    Turret_Shooter_Master.config().set_kD(0.04);
-    Turret_Shooter_Master.config().set_kF(0.047651);
-    Turret_Shooter_Master.config().set_closed_loop_ramp(2.5);
-    Turret_Shooter_Master.config().set_peak_output_reverse(0.3);
+    Turret_Shooter_Master->config().set_kP(0.03);
+    Turret_Shooter_Master->config().set_kI(0.0);
+    Turret_Shooter_Master->config().set_kD(0.04);
+    Turret_Shooter_Master->config().set_kF(0.047651);
+    Turret_Shooter_Master->config().set_closed_loop_ramp(2.5);
+    Turret_Shooter_Master->config().set_peak_output_reverse(0.3);
 }
 
 int main(int argc, char **argv)
@@ -65,7 +71,9 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	node = &n;
 
-    node->subscribe("/HMI_Signals", 20, hmi_signal_callback);
+    config_motors();
+
+    ros::Subscriber hmi_subscribe = node->subscribe("/HMI_Signals", 20, hmi_signal_callback);
 
 	ros::spin();
 	return 0;
