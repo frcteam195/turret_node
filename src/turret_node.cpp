a772463e0fab54f34102613243e7421563dc5c0e
Michael Todd <eltodd@gmail.com>
2022-03-17 22:26:04 +0000

    Update limelight to always be on

--- src/turret_node.cpp
+++ src/turret_node.cpp
@@ -209,7 +209,7 @@ void turn_limelight_off()
 
     limelight_vision_node::Limelight limelight;
     limelight.name = "limelight";
-    limelight.pipeline = 0;
+    limelight.pipeline = 1;
 
     limelight_vision_node::Limelight_Control limelight_control;
     limelight_control.limelights.push_back(limelight);
